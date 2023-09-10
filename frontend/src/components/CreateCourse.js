f916ec1f17349b2b303311c9883b2144555a7a82
it21096570 <it21096570@my.sliit.lk>
2023-09-10 16:47:23 +0530

    push

--- frontend/src/components/CreateCourse.js
+++ frontend/src/components/CreateCourse.js
@@ -1,10 +1,8 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios'; // You may need to install axios if not already done
-
+import axios from 'axios';
 
 function CreateCourse() {
-
   const navigate = useNavigate();
 
   const [courseData, setCourseData] = useState({
@@ -12,6 +10,8 @@ function CreateCourse() {
     coursename: '',
     description: '',
   });
+
+  const [errors, setErrors] = useState({});
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -24,6 +24,23 @@ function CreateCourse() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    // Validate inputs
+    const validationErrors = {};
+    if (!courseData.courseid.trim()) {
+      validationErrors.courseid = 'Course ID is required';
+    }
+    if (!courseData.coursename.trim()) {
+      validationErrors.coursename = 'Course Name is required';
+    }
+    if (!courseData.description.trim()) {
+      validationErrors.description = 'Description is required';
+    }
+
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      return;
+    }
+
     try {
       // Send a POST request to your server to add the course
       await axios.post('http://localhost:8080/courses/add', courseData);
@@ -35,54 +52,70 @@ function CreateCourse() {
         coursename: '',
         description: '',
       });
-      navigate('/getCourseAdmin/')
-
-
+      navigate('/getCourseAdmin/');
     } catch (error) {
       // Handle errors, e.g., display an error message
       console.error('Error adding course:', error);
     }
   };
 
-  
   return (
     <div className="min-h-screen flex items-center justify-center bg-themeLightGray">
       <div className="bg-white p-8 rounded shadow-md w-96">
         <h2 className="text-2xl font-bold text-themeBlue mb-4">Create a New Course</h2>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label htmlFor="courseid" className="text-themeBlue">Course ID:</label>
+            <label htmlFor="courseid" className="text-themeBlue">
+              Course ID:
+            </label>
             <input
               type="text"
               id="courseid"
               name="courseid"
               value={courseData.courseid}
               onChange={handleChange}
-              className="w-full px-3 py-2 border rounded-md border-themePurple focus:outline-none focus:border-themeBlue"
-              required
+              className={`w-full px-3 py-2 border rounded-md border-themePurple focus:outline-none focus:border-themeBlue ${
+                errors.courseid ? 'border-red-500' : ''
+              }`}
             />
+            {errors.courseid && (
+              <p className="text-red-500 text-xs mt-1">{errors.courseid}</p>
+            )}
           </div>
           <div className="mb-4">
-            <label htmlFor="coursename" className="text-themeBlue">Course Name:</label>
+            <label htmlFor="coursename" className="text-themeBlue">
+              Course Name:
+            </label>
             <input
               type="text"
               id="coursename"
               name="coursename"
               value={courseData.coursename}
               onChange={handleChange}
-              className="w-full px-3 py-2 border rounded-md border-themePurple focus:outline-none focus:border-themeBlue"
-              required
+              className={`w-full px-3 py-2 border rounded-md border-themePurple focus:outline-none focus:border-themeBlue ${
+                errors.coursename ? 'border-red-500' : ''
+              }`}
             />
+            {errors.coursename && (
+              <p className="text-red-500 text-xs mt-1">{errors.coursename}</p>
+            )}
           </div>
           <div className="mb-4">
-            <label htmlFor="description" className="text-themeBlue">Description:</label>
+            <label htmlFor="description" className="text-themeBlue">
+              Description:
+            </label>
             <textarea
               id="description"
               name="description"
               value={courseData.description}
               onChange={handleChange}
-              className="w-full px-3 py-2 border rounded-md border-themePurple focus:outline-none focus:border-themeBlue"
+              className={`w-full px-3 py-2 border rounded-md border-themePurple focus:outline-none focus:border-themeBlue ${
+                errors.description ? 'border-red-500' : ''
+              }`}
             />
+            {errors.description && (
+              <p className="text-red-500 text-xs mt-1">{errors.description}</p>
+            )}
           </div>
           <div>
             <button
